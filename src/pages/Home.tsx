11c642e1abe50bef25947ab37bf25c11aed0cd23
aryeeeeeeeeel <arielsumantin69@gmail.com>
2025-05-05 14:12:46 +0800

    Removed Home.css and Configured Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+
+import { 
+  IonButton,
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonIcon, 
+  IonLabel, 
+  IonMenuButton, 
+  IonPage, 
+  IonRouterOutlet, 
+  IonTabBar, 
+  IonTabButton, 
+  IonTabs, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+
+import { IonReactRouter } from '@ionic/react-router';
+import { bookOutline, search, star } from 'ionicons/icons';
+import { Route, Redirect } from 'react-router';
+import Feed from './home-tabs/Feed';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+
+  const tabs = [
+    {name:'Feed', tab:'feed',url: '/it35-final/app/home/feed', icon: bookOutline},
+  ]
+  
+return (
+  <IonReactRouter>
+        <IonTabs>
+          <IonTabBar slot="bottom">
+            {tabs.map((item, index) => (
+              <IonTabButton key={index} tab={item.tab} href={item.url}>
+                <IonIcon icon={item.icon} />
+                <IonLabel>{item.name}</IonLabel>
+              </IonTabButton>
+            ))}
+            
+          </IonTabBar>
+        <IonRouterOutlet>
+          <Route exact path="/it35-final/app/home/feed" render={Feed} />
+          <Route exact path="/it35-final/app/home">
+            <Redirect to="/it35-final/app/home/feed" />
+          </Route>
+        </IonRouterOutlet>
+        </IonTabs>
+      </IonReactRouter>
   );
 };
 
