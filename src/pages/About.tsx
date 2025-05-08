d9c88db2d2de2af0d8f4eb3b93931c853b466e4a
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-05-08 11:36:31 +0800

    added contents in about.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,9 @@
 import { 
     IonButtons,
+      IonCard,
+      IonCardContent,
+      IonCardHeader,
+      IonCardTitle,
       IonContent, 
       IonHeader, 
       IonMenuButton, 
@@ -20,6 +24,16 @@ import {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Emergency Status Logger</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            The Emergency Status Logger is a lightweight and user-friendly mobile web application designed to quickly report an individual’s safety status during an emergency. Users can mark themselves as "Safe" or indicate they "Need Help" with just a single tap, making it ideal for fast responses in critical situations. The system then updates real-time statistics to inform responders and administrators of current conditions and needs.
+
+            This app is built using Ionic React and integrated with Supabase for secure backend storage and real-time data synchronization. Its simple interface ensures accessibility for all users, while the responsive design makes it suitable for both mobile and desktop environments. Whether for disaster response or community safety coordination, this app aims to provide a reliable platform for emergency communication.
+          </IonCardContent>
+          </IonCard>
         </IonContent>
       </IonPage>
     );
